6abd1162dd762281857073a285c74adb4270b072
Sharonne24 <sawuor395@gmail.com>
2023-07-26 18:03:23 +0300

    Add files via upload

---
+++ application_controller.rb
@@ -0,0 +1,52 @@
+class ApplicationController < Sinatra::Base
+    set :default_content_type, 'application/json'
+    
+    #get all events
+    get "/events" do
+     events = Event.all
+     events.to_json
+    end
+  
+    # Add a new event
+    post "/events" do
+       event = Event.create(name: params[:name], datetime: params[:datetime], location: params[:location], description: params[:description])
+      status 201
+      event.to_json
+    end
+
+    #delete and event 
+    delete "/events/:id" do
+      event = Event.find(params[:id])
+      if event.destroy
+        status 204
+      else
+        status 422
+        { error: "Failed to delete the event" }.to_json
+      end
+    end
+
+    #get a single event by id
+    get "/events/:id" do
+      event = Event.find(params[:id])
+      event.to_json
+    end
+
+    #update an existing event
+    put "/events/:id" do
+      event = Event.find(params[:id])
+      request_body = JSON.parse(request.body.read)
+      if event.update(request_body)
+        event.to_json
+      else
+        status 422
+        { error: "Failed to update the event" }.to_json
+      end
+    end
+
+    #signing up for an event
+    post "/signup" do
+      user = User.create(email: params[:email], password_digest: params[:password_digest])
+      user.to_json
+    end
+  end
+  
